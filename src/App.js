c2dd46254b14ec77f0ed819a39bf7b24140922d2
Sion Fandrick <github@sions.org>
2022-05-07 11:12:23 -0600

    Created Basic Spell Cards

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import spellFile from './spells-ww.json';
 import './App.css';
+
+function SpellCard(props) {
+    return (
+      <div className='Card'>
+        <h2>{props.entry.name}</h2> - <span>{props.entry.description}</span>
+        <div className='CardStats'>
+          <div className='CardStatsItem'><p>Level: </p>{props.entry.level}</div>
+          <div className='CardStatsItem'><p>Casting Time: </p>{props.entry.castingTime}</div>
+          <div className='CardStatsItem'><p>Range/Area: </p>{props.entry.range}</div>
+          <div className='CardStatsItem'><p>Duration: </p>{props.entry.duration}</div>
+          <div className='CardStatsItem'><p>School: </p>{props.entry.school}</div>
+          {props.entry.tags &&
+            <div>Tags: {props.entry.tags}</div>
+          }
+        </div>
+        <div>
+          {props.entry.entry.map((line) => 
+          <p>{line}</p>
+          )}
+          {props.entry.entryHigherLevel &&
+          <div>
+            <b>At Higher Levels:</b>
+            <p>{props.entry.entryHigherLevel}</p>
+          </div>
+          }
+            
+        </div>
+      </div>
+    )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        <div>
+          {spellFile.map((entry) => 
+            <SpellCard key={entry.name} entry={entry} />
+          )}
+        </div>
+      </ul>
     </div>
   );
 }
