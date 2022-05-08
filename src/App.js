a3ee7f13f4b4f8d637132b0c0589194aab93c6ba
Sion Fandrick <github@sions.org>
2022-05-08 07:28:23 -0600

    Implemented search on Spell Names

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
 import spellFile from './spells-ww.json';
 import SpellListing from './components/SpellCard';
+import Search from './components/Search'
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [word, setWord] = useState("");
+  const [filterDisplay, setFilterDisplay] = useState([]);
+
+  const handleChange = e => {
+    setWord(e);
+    let oldList = spellFile;
+
+    if (word !== "") {
+      let newList = [];
+
+      newList = oldList.filter(spell =>
+        spell.name.toLowerCase().includes(word.toLowerCase())
+      );
+
+      setFilterDisplay(newList);
+    } else {
+      setFilterDisplay(oldList)
+    }
+
+  };
+
   return (
     <div className="App">
-      <ul>
-        <div>
-          {
-            <SpellListing list={spellFile} />
-          }
-        </div>
-      </ul>
+      <Search value={word} handleChange={ e => handleChange(e.target.value)} />
+      <SpellListing list={word.length < 1 ? spellFile : filterDisplay} />
     </div>
   );
 }
