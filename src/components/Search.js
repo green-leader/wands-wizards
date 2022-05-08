7fe34ea359bdee23bd7cf7555e8b0b1f0ad58320
Sion Fandrick <github@sions.org>
2022-05-08 07:32:30 -0600

    Implemented basic search on spell name

---
+++ src/components/Search.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Search = ({ value, handleChange }) => {
+    return (
+        <div>
+            Search: <input value={value} onChange={handleChange} />
+        </div>
+    );
+};
+
+export default Search;
