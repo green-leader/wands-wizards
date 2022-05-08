df5312a42a6648bfa5a657a37c81a2e6bf24b42b
Sion Fandrick <github@sions.org>
2022-05-08 07:08:11 -0600

    Refactored spell listing to a separete component.

--- src/App.js
+++ src/App.js
@@ -1,44 +1,15 @@
 import spellFile from './spells-ww.json';
+import SpellListing from './components/SpellCard';
 import './App.css';
-
-function SpellCard(props) {
-    return (
-      <div className='Card'>
-        <h2>{props.entry.name}</h2> - <span>{props.entry.description}</span>
-        <div className='CardStats'>
-          <div className='CardStatsItem'><p>Level: </p>{props.entry.level}</div>
-          <div className='CardStatsItem'><p>Casting Time: </p>{props.entry.castingTime}</div>
-          <div className='CardStatsItem'><p>Range/Area: </p>{props.entry.range}</div>
-          <div className='CardStatsItem'><p>Duration: </p>{props.entry.duration}</div>
-          <div className='CardStatsItem'><p>School: </p>{props.entry.school}</div>
-          {props.entry.tags &&
-            <div>Tags: {props.entry.tags}</div>
-          }
-        </div>
-        <div>
-          {props.entry.entry.map((line) => 
-          <p>{line}</p>
-          )}
-          {props.entry.entryHigherLevel &&
-          <div>
-            <b>At Higher Levels:</b>
-            <p>{props.entry.entryHigherLevel}</p>
-          </div>
-          }
-            
-        </div>
-      </div>
-    )
-}
 
 function App() {
   return (
     <div className="App">
       <ul>
         <div>
-          {spellFile.map((entry) => 
-            <SpellCard key={entry.name} entry={entry} />
-          )}
+          {
+            <SpellListing list={spellFile} />
+          }
         </div>
       </ul>
     </div>
