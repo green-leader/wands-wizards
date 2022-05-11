31d455191e6b5f498497493d170e7b29f6221d20
Sion <github@sions.org>
2022-05-11 13:19:39 -0600

    Added Prettier files, and ran it.

--- src/App.js
+++ src/App.js
@@ -1,34 +1,33 @@
-import spellFile from './spells-ww.json';
-import SpellListing from './components/SpellCard';
-import Search from './components/Search'
-import './App.css';
-import { useState } from 'react';
+import spellFile from "./spells-ww.json";
+import SpellListing from "./components/SpellCard";
+import Search from "./components/Search";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
   const [word, setWord] = useState("");
   const [filterDisplay, setFilterDisplay] = useState([]);
 
-  const handleChange = e => {
+  const handleChange = (e) => {
     setWord(e);
     let oldList = spellFile;
 
     if (word !== "") {
       let newList = [];
 
-      newList = oldList.filter(spell =>
+      newList = oldList.filter((spell) =>
         spell.name.toLowerCase().includes(word.toLowerCase())
       );
 
       setFilterDisplay(newList);
     } else {
-      setFilterDisplay(oldList)
+      setFilterDisplay(oldList);
     }
-
   };
 
   return (
     <div className="App">
-      <Search value={word} handleChange={ e => handleChange(e.target.value)} />
+      <Search value={word} handleChange={(e) => handleChange(e.target.value)} />
       <SpellListing list={word.length < 1 ? spellFile : filterDisplay} />
     </div>
   );
